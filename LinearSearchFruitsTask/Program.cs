e90b61498dd735e379768703d09a5620137bfefd
Al Stone <astone@heckgrammar.co.uk>
2024-06-28 12:09:32 +0100

    Add project files.

---
+++ LinearSearchFruitsTask/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LinearSearchFruitsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
+
+            // Your program should:
+            // Ask the user what word they would like to find
+            // Output the message True if the word is found
+            // Output the message False if the word is not found
+            
+            // You must write your own linear search routine and not use any built-in fuctions available in C#
+            // You should use meaningful variable names
+        }
+    }
+}
